a198c21d095043e91aee1631518a4bd13920a4f6
stejerina2911 <A00832806@tec.mx>
2024-11-05 23:07:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from streamlit_shap import st_shap
 st.set_page_config(page_title="Predicción de Probabilidad de Empleo", layout="centered")
 
 # Cargar el modelo
-rf = joblib.load('random_forest_model.joblib')
+rf = joblib.load('random_forest_modelSTM.joblib')
 
 st.title("🧑‍💼 Predicción de Probabilidad de Empleo")
 
